d7f91a93ed90036060f7df70d2f80271c11e8481
vboudis <v.boudis1995@gmail.com>
2021-05-21 17:27:11 +0300

    Pageable documentation applied

--- Core/src/main/java/com/evangelos/couchbase/lite/core/Pageable.kt
+++ Core/src/main/java/com/evangelos/couchbase/lite/core/Pageable.kt
@@ -3,17 +3,31 @@ package com.evangelos.couchbase.lite.core
 import com.couchbase.lite.Expression
 import com.couchbase.lite.Ordering
 
+/**
+ * A class that holds information for paging.
+ * @param pageNo The number of the page. Starts from 0
+ * @param pageSize The size of the page. Starts from 1
+ * @param order A Map for ordering.
+ *      Key: represents the field name
+ *      Value: represents the order. True -> Ascending, False -> Descending
+ */
 data class Pageable(
     val pageNo: Int,
     val pageSize: Int,
     val order: Map<String, Boolean>
 ) {
 
+    /**
+     * Offset used by [com.couchbase.lite.OrderBy.limit]
+     * */
     val offset: Int
         get() {
             return pageSize + pageNo
         }
 
+    /**
+     * Ordering used by [com.couchbase.lite.Where.orderBy]
+     * */
     val ordering: Array<Ordering>
         get() {
             return order.map { entry ->

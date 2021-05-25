fe4b27d0c9e357fcabdf9be212520f5a7c792eec
vboudis <v.boudis1995@gmail.com>
2021-05-25 16:00:52 +0300

    Turn Core package into Java-Kotlin library

--- app/src/main/java/com/evangelos/couchbase/lite/dao/MainActivity.kt
+++ app/src/main/java/com/evangelos/couchbase/lite/dao/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.couchbase.lite.CouchbaseLite
 
+// TODO: Catch exceptions inside ViewModels
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
